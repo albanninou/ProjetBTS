6bd55f93e3dd6a222c907bf41e467954efaa3443
Unknown <albanninou@gmail.com>
2018-03-29 08:09:45 +0200

    Clean and update mindview

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -8,6 +8,8 @@
 
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+// master:modefonctionnement/couleur
+//master:1/1
 
 
 int mode = -1;
@@ -19,15 +21,14 @@ String question = "master?";
 int numOfCard = 0;
 boolean needUpdate = false;
 boolean masterState = false;
+boolean masterUpdate = false;
 String tick = "tick";
 int count = 0;
 int connectedCard = 0;
 String needToSend = "";
 
 void setup() {
-  
   strip.begin();
-  
   strip.show();
   Serial2.begin(115200);
   Serial.begin(9600);
@@ -36,35 +37,32 @@ void setup() {
   delay(1000);
   Serial2.println(question);
   delay(1000);
-  while(Serial2.available() > 0) {           
-      String trame = Serial2.readString();
-      char buf[trame.length()];
-      trame.toCharArray(buf, sizeof(buf));
-      const char s[2] = "\n";
-      char *token;
-
-      token = strtok(buf, s);
-      while(token != NULL){
-          String test = token;
-          Serial.println("-------");
-          Serial.println(test);
-          int firstListItem = test.indexOf(you);
-          if(firstListItem != -1){
-            test = test.substring(firstListItem);
-            test = test.substring(you.length());
-            Serial.println(test);
-            numOfCard = test.toInt();
-          }
-         
-          token = strtok(NULL, s);
-     }
-          
+  while (Serial2.available() > 0) {
+    String trame = Serial2.readString();
+    char buf[trame.length()];
+    trame.toCharArray(buf, sizeof(buf));
+    const char s[2] = "\n";
+    char *token;
+    token = strtok(buf, s);
+    while (token != NULL) {
+      String test = token;
+      Serial.println("-------");
+      Serial.println(test);
+      int firstListItem = test.indexOf(you);
+      if (firstListItem != -1) {
+        test = test.substring(firstListItem);
+        test = test.substring(you.length());
+        Serial.println(test);
+        numOfCard = test.toInt();
+      }
+      token = strtok(NULL, s);
+    }
   }
-  if(numOfCard == 0){
+  if (numOfCard == 0) {
     mode = 0;
     color = 0;
     eneable = true;
-  }else{
+  } else {
     mode = 1;
     color = 2;
     eneable = true;
@@ -74,193 +72,169 @@ void setup() {
 }
 
 void loop() {
-  while(Serial2.available() > 0) {           
-        String trame = Serial2.readString();
-        char buf[trame.length()];
-      trame.toCharArray(buf, sizeof(buf));
-      const char s[2] = "\n";
-      char *token;
-
-      token = strtok(buf, s);
-      while(token != NULL){
-          String test = token;
-          int firstListItem = test.indexOf(master);
-          if(firstListItem != -1){
-            test = test.substring(firstListItem);
-            test = test.substring(master.length());
-            setState(test);
-          }
-          firstListItem = test.indexOf(question);
-          if(firstListItem != -1 && numOfCard == 0){
-            connectedCard++;
-            needToSend +=you;
-            needToSend+=connectedCard;
-            needToSend+="\n";
-          }
-          firstListItem = test.indexOf(tick);
-          if(firstListItem != -1 && numOfCard != 0){
-            
-            test = test.substring(firstListItem);
-           
-            test = test.substring(tick.length()+1);
- 
-            char buf25[sizeof(test)];
-            test.toCharArray(buf25, sizeof(buf25));
-            
-            int d = atoi(buf25);
-            if(d == 0){
-             numOfCard = 0;
-              masterState = false;
-            }else{
-              masterState = true;
-             numOfCard = 0;
-            }
-            sendEtat();
-          }
-         
-        token = strtok(NULL, s);
-     }
+  while (Serial2.available() > 0) {
+    String trame = Serial2.readString();
+    char buf[trame.length()];
+    trame.toCharArray(buf, sizeof(buf));
+    const char s[2] = "\n";
+    char *token;
+    token = strtok(buf, s);
+    while (token != NULL) {
+      String test = token;
+      int firstListItem = test.indexOf(master);
+      if (firstListItem != -1) {
+        test = test.substring(firstListItem);
+        test = test.substring(master.length());
+        setState(test);
+      }
+      firstListItem = test.indexOf(question);
+      if (firstListItem != -1 && numOfCard == 0) {
+        connectedCard++;
+        needToSend += you;
+        needToSend += connectedCard;
+        needToSend += "\n";
+      }
+      firstListItem = test.indexOf(tick);
+      if (firstListItem != -1 && numOfCard != 0) {
+        test = test.substring(firstListItem);
+        test = test.substring(tick.length() + 1);
+        char buf25[sizeof(test)];
+        test.toCharArray(buf25, sizeof(buf25));
+        int d = atoi(buf25);
+        if (d == 0) {
+          numOfCard = 0;
+          masterState = false;
+        } else {
+          masterState = true;
+          numOfCard = 0;
+        }
+        masterUpdate = true;
+        sendEtat();
+      }
+      token = strtok(NULL, s);
+    }
   }
-    
-    strip.setBrightness(BRIGHTNESS);
-    if(numOfCard == 0){
-      count++;
-    }
-        switch(mode){
-          case -1:
-           
-            
-            if(count >= NUMCOUNT || masterState){
-               blank();
-               sendEtat();
-               masterState = false;
-               if(numOfCard == 0){
-               count = 0;
-               }
-            }
-          
-          break;
-          case 0:
-          
-            
-           
-            if(count >= NUMCOUNT || masterState){
-                sendEtat();
-               setColor();
-               masterState = false;
-               if(numOfCard == 0){
-               count = 0;
-               }
-            }
-          
-          break;
-           case 1:
-            
-            if(count >= NUMCOUNT|| masterState){
-                masterState = false;
-               eneable = !eneable;
-               if(eneable){
-                setColor();
-               }else{
-                blank();
-                
-               }
-               sendEtat();
-               if(numOfCard == 0){
-               count = 0;
-               }
-            }
-          break;
-          
+  strip.setBrightness(BRIGHTNESS);
+  if (numOfCard == 0) {
+    count++;
+  }
+  switch (mode) {
+    case -1:
+      if (count >= NUMCOUNT   || masterUpdate) {
+        masterUpdate = false;
+        blank();
+        sendEtat();
+        count = 0;
+      }
+      break;
+    case 0:
+      if (count >= NUMCOUNT   || masterUpdate) {
+        if (numOfCard != 0) {
+          eneable = masterState;
         }
-        
-        if(needToSend != ""){
-          
-          Serial2.print(needToSend);
-          needToSend="";
-          
+        sendEtat();
+        masterUpdate = false;
+        setColor();
+        count = 0;
+      }
+      break;
+    case 1:
+
+      if (count >= NUMCOUNT || masterUpdate) {
+        masterUpdate = false;
+        if (numOfCard != 0) {
+          eneable = masterState;
+        } else {
+          eneable = !eneable;
         }
-  
+        sendEtat();
+        if (eneable) {
+          setColor();
+        } else {
+          blank();
+        }
+        count = 0;
+      }
+      break;
+  }
+  if (needToSend != "") {
+    Serial2.print(needToSend);
+    needToSend = "";
+  }
 }
 
- void setColor(){
-    switch(color){
-      case 0:
-        red();
+void setColor() {
+  switch (color) {
+    case 0:
+      red();
       break;
-      case 1:
-        green();
+    case 1:
+      green();
       break;
-      case 2:
-        blue();
+    case 2:
+      blue();
       break;
-    }
+  }
 }
 
-void sendEtat(){
+void sendEtat() {
   char str[20];
-  if(numOfCard == 0){
-    if(eneable){
-      needToSend+=tick+"/1"+"\n";
-    }else{
-      needToSend+=tick+"/0"+"\n";
+  if (numOfCard == 0) {
+    if (eneable) {
+      needToSend += tick + "/1" + "\n";
+    } else {
+      needToSend += tick + "/0" + "\n";
     }
   }
-  if(eneable){
-    sprintf(str, "%d/1/%d", numOfCard,color);
-  }else{
-    sprintf(str, "%d/0/%d", numOfCard,color);
+  if (eneable) {
+    sprintf(str, "%d/1/%d", numOfCard, color);
+  } else {
+    sprintf(str, "%d/0/%d", numOfCard, color);
   }
-  needToSend+=str;
-  needToSend+="\n";
+  needToSend += str;
+  needToSend += "\n";
 }
 
- void blank(){
-for (int i = 0; i < NUMPIXELS; i++) {
+void blank() {
+  for (int i = 0; i < NUMPIXELS; i++) {
     strip.setPixelColor(i, 0, 0, 0);
     strip.show();
   }
 }
 
- void red(){
-for (int i = 0; i < NUMPIXELS; i++) {
+void red() {
+  for (int i = 0; i < NUMPIXELS; i++) {
     strip.setPixelColor(i, 255, 0, 0);
     strip.show();
   }
 }
- void blue(){
-for (int i = 0; i < NUMPIXELS; i++) {
+void blue() {
+  for (int i = 0; i < NUMPIXELS; i++) {
     strip.setPixelColor(i, 0, 0, 255);
     strip.show();
   }
 }
- void green(){
-for (int i = 0; i < NUMPIXELS; i++) {
+void green() {
+  for (int i = 0; i < NUMPIXELS; i++) {
     strip.setPixelColor(i, 0, 255, 0);
     strip.show();
   }
 }
 
-void setState(String trame){
-char buf[sizeof(trame)];
-            trame.toCharArray(buf, sizeof(buf));
-            const char s[2] = "/";
-            char *token;
-             
-            token = strtok(buf, s);
-            if(token !=NULL){
-              mode = atoi( token );
-             
-            }
-            Serial.print("Mode :" );
-            Serial.println(mode );
-            token = strtok(NULL, s);
-            if(token !=NULL){
-              color = atoi( token );
-              
-            }
-            eneable = true;
-            needUpdate = true;
+void setState(String trame) {
+  char buf[sizeof(trame)];
+  trame.toCharArray(buf, sizeof(buf));
+  const char s[2] = "/";
+  char *token;
+  token = strtok(buf, s);
+  if (token != NULL) {
+    mode = atoi( token );
+  }
+  token = strtok(NULL, s);
+  if (token != NULL) {
+    color = atoi( token );
+  }
+  eneable = true;
+  needUpdate = true;
 }
-
 
--- Project/app/src/main/java/com/parkflash/project/DeviceManager.java
+++
@@ -1,8 +0,0 @@
-
-package com.parkflash;
-
-/**
- * Created by alban on 22/01/2018.
- */
-public class DeviceManager {
-}
